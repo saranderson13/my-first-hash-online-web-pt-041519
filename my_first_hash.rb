ef2139779201677799a8fe84d8fa71d88fa0aa11
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 03:33:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
   
+  my_hash = { 
+    :in_the_sky => ["stars", "moon"]
+    :under_the_sea => ["mermaids", "scary fish"]
+  }
 end
 
 
