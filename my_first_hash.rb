d2e574644e404edd38638f1c0b6f07527245a82b
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 03:39:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,8 +2,8 @@ def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   
   my_hash = { 
-    :in_the_sky => ["stars", "moon"],
-    :under_the_sea => ["mermaids", "scary fish"]
+    in_the_sky: ["stars", "moon"],
+    under_the_sea: ["mermaids", "scary fish"]
   }
 end
 
@@ -33,17 +33,16 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+  }
+  
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
 
-
   # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
-
 
   # return the shipping_manifest hash below
   shipping_manifest
